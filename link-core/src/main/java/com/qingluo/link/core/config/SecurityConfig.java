f1460224fbc28cd3b25c6af171a4384f549dc520
方一帆 <15014170+fang-yifan-not-ready@user.noreply.gitee.com>
2026-04-14 23:34:05 +0800

    修改了一下权限安全配置类

--- link-core/src/main/java/com/qingluo/link/core/config/SecurityConfig.java
+++ link-core/src/main/java/com/qingluo/link/core/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.qingluo.link.core.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -9,10 +11,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * 安全配置
  */
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/api/v1/auth/**").permitAll()
+            .anyRequest().authenticated();
     }
 }
